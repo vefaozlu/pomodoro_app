96e91a26a50b941a9162b19fb7f0b5c789f28082
vefaozlu <vefaozlu01@gmail.com>
2022-04-14 16:41:35 +0300

    remove unnecessary widgets

--- lib/view/widgets/goal_tile.dart
+++ lib/view/widgets/goal_tile.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pomodoro_app/colors.dart';
-import 'package:pomodoro_app/view/screens/screens.dart';
 import 'package:pomodoro_app/view_model/goals_bloc/goals_bloc.dart';
 import 'package:pomodoro_app/model/models/models.dart';
 
-class A extends StatelessWidget {
-  const A({Key? key, required this.goal, required this.goToInitialPage, required this.pushGoalInputPage})
+class GoalTile extends StatelessWidget {
+  const GoalTile({Key? key, required this.goal, required this.goToInitialPage, required this.pushGoalInputPage})
       : super(key: key);
 
   final Goal goal;
@@ -77,127 +76,4 @@ class A extends StatelessWidget {
   }
 }
 
-class GoalTile extends StatefulWidget {
-  const GoalTile({Key? key, required this.goal, required this.goToInitialPage})
-      : super(key: key);
 
-  final Goal goal;
-  final VoidCallback goToInitialPage;
-
-  @override
-  State<GoalTile> createState() => _GoalTileState();
-}
-
-class _GoalTileState extends State<GoalTile> {
-  bool isDeleted = false;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Dismissible(
-        key: Key('${widget.goal.id}'),
-        direction: DismissDirection.startToEnd,
-        onDismissed: (direction) => context
-            .read<GoalsBloc>()
-            .add(DeleteGoal(goalToDelete: widget.goal)),
-        child: SizedBox(
-          height: 120,
-          width: MediaQuery.of(context).size.width * .8,
-          child: Container(
-            height: 100,
-            width: MediaQuery.of(context).size.width * .8,
-            decoration: const BoxDecoration(
-              color: Colors.deepOrangeAccent,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: () async {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          GoalInputPage(goalToEdit: widget.goal),
-                    ),
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Stack(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              widget.goal.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '${widget.goal.pomodorosDone} of ${widget.goal.estimatedPomodoros} done',
-                              style: const TextStyle(fontSize: 21),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              icon: const Icon(
-                                Icons.play_arrow_rounded,
-                                size: 32,
-                              ),
-                              onPressed: () {
-                                context.read<GoalsBloc>().add(
-                                      ChangeCurrentGoal(
-                                          newCurrentGoal: widget.goal),
-                                    );
-                                widget.goToInitialPage();
-                              },
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete),
-                              onPressed: () => context.read<GoalsBloc>().add(
-                                    DeleteGoal(goalToDelete: widget.goal),
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-}
